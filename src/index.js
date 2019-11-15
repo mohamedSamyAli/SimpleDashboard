f9e1bd49fcdaa6e2c181c226818c94ca55f67c56
mohamedSamyAli <msamy2019@gmail.com>
2019-11-15 16:22:12 +0200

    vloro2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
-// import store from "./Store/store"
 import {createStore} from "redux"
 import reducer from "./Store/User"
 
